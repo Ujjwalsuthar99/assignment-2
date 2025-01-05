b446f72e4bee496c30bfa61e3b484a40c464daf1
Ujjwalsuthar99 <130583600+Ujjwalsuthar99@users.noreply.github.com>
2025-01-05 18:30:54 +0530

    Update AuthorService.java

--- src/main/java/com/example/assignment/service/AuthorService.java
+++ src/main/java/com/example/assignment/service/AuthorService.java
@@ -6,4 +6,7 @@ import com.example.assignment.persistence.entity.AuthorEntity;
 public interface AuthorService {
     AuthorEntity getAuthorById(Long id);
     Long createAuthor(AuthorRequestDTO authorRequestDTO);
+    void deleteAuthor(Long authorId);
+    Long updateAuthor(Long authorId, AuthorRequestDTO authorRequestDTO);
+
 }
