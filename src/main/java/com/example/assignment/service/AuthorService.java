085a4e4e81355a872b28306576f21af2829712a2
Ujjwalsuthar99 <130583600+Ujjwalsuthar99@users.noreply.github.com>
2025-01-05 18:31:10 +0530

    Update AuthorService.java

--- src/main/java/com/example/assignment/service/AuthorService.java
+++ src/main/java/com/example/assignment/service/AuthorService.java
@@ -1,12 +1,12 @@
 package com.example.assignment.service;
 
-import com.example.assignment.persistence.dto.AuthorRequestDTO;
-import com.example.assignment.persistence.entity.AuthorEntity;
+import com.example.assignment.persistence.dto.BookRequestDTO;
+import com.example.assignment.persistence.entity.BookEntity;
 
-public interface AuthorService {
-    AuthorEntity getAuthorById(Long id);
-    Long createAuthor(AuthorRequestDTO authorRequestDTO);
-    void deleteAuthor(Long authorId);
-    Long updateAuthor(Long authorId, AuthorRequestDTO authorRequestDTO);
 
+public interface BookService {
+    BookEntity getBookById(Long id);
+    Long createBook(BookRequestDTO bookRequestDTO);
+    void deleteBook(Long bookId);
+    Long updateBook(Long bookId, BookRequestDTO bookRequestDTO);
 }
